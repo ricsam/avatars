7774c5d798199f7050053f45a4cc2ad4845a0255
Richard Samuelsson <noobtoothfairy@gmail.com>
2020-08-12 13:21:16 +0200

    use heroku port

--- api/src/index.ts
+++ api/src/index.ts
@@ -114,7 +114,7 @@ app.use(express.static(path.join(__dirname, '../../web/build')))
 
 server.applyMiddleware({ app });
 
-// bodyParser is needed just for POST.
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+const port = process.env.PORT || 4000;
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
 );
